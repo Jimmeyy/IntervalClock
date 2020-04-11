cb3f7d025265d47e0bcbd3215601f9660e7afa56
Damian Zmuda <d.zmuda94@gmail.com>
2020-04-11 13:04:53 +0200

    Update inputs placeholders and update mobile menu logic

--- javascript/mobileMenu.js
+++ javascript/mobileMenu.js
@@ -1,3 +1,6 @@
+import selectors from './selectors';
+import validation from './validation';
+
 const mobileMenu = function () {
     function isMobile() {
         if (window.innerWidth < 1201) {
@@ -7,19 +10,28 @@ const mobileMenu = function () {
     }
 
     if (isMobile()) {
+        const { btns } = selectors();
+
         const menu = {
             main: document.querySelector('.side-menu-js'),
             openBtn: document.querySelector('.side-menu-open-js'),
-            closeBtn: document.querySelector('.side-menu-close-js')
-        }
+            closeBtn: document.querySelector('.side-menu-close-js'),
+        };
 
         function handleSideMenuToggle() {
             menu.main.classList.toggle('is-active');
         }
 
+        function startBtnMobileHandler() {
+            if (!validation()) {
+                handleSideMenuToggle();
+            }
+        }
+
         menu.openBtn.addEventListener('click', handleSideMenuToggle);
         menu.closeBtn.addEventListener('click', handleSideMenuToggle);
+        btns.start.addEventListener('click', startBtnMobileHandler);
     }
-}
+};
 
 export default mobileMenu();
